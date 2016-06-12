2261b0280f2a2bd76fce5307cbc70f7dfbe22cd9
ervitis <vitomarti@gmail.com>
2016-06-12 19:42:38 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bdirFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bdirFrontApp
+ */
+angular.module('bdirFrontApp')
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
